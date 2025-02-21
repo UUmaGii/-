3a421bfe19d746e32551af9257dc4f068065e0fd
code崽 <1023844420@qq.com>
2025-02-22 00:34:02 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vitepress/dist
+
+# 初始化 Git 仓库并提交更改
+git init
+git add -A
+git commit -m "deploy"
+
+# 推送到 gh-pages 分支
+git branch -M main
+git remote add origin git@github.com:你的GitHub用户名/你的GitHub仓库.git
+git push -f origin main:gh-pages
+
+cd -
